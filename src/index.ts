58b4253e7fda5c454805786c6de5b9634b01dcf0
Roberto Sousa <robertosousa186@gmail.com>
2022-11-19 00:11:46 -0300

    Creating a simple server to test dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello world!');
+import express from 'express';
+import { Request, Response } from 'express';
+
+const app = express();
+
+const PORT = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello world!!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
