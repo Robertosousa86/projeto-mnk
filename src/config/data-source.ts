b966b3f61a4a4bcf65fbe63da201ce6a086b33df
Roberto Sousa <robertosousa186@gmail.com>
2022-11-21 20:51:39 -0300

    Configuring data-source with environment variables

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -3,13 +3,19 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const AppDataSource = new DataSource({
+export const AppDataSource = new DataSource({
   type: 'mysql',
   host: process.env.DATABASE_HOST,
-  port: +process.env.DB_PORT,
+  port: +process.env.DATABASE_PORT,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE,
+  logging: true,
+  synchronize: true,
+  entities: [
+    /*...*/
+  ],
+  migrations: ['src/migrations/**/*{.ts,.js}'],
 });
 
 AppDataSource.initialize()
