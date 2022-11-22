abaefdd8879d6fb7cb7d4d241b636b9dde75a574
Roberto Sousa <robertosousa186@gmail.com>
2022-11-21 22:32:48 -0300

    Updating migration

--- src/migrations/1669075905477-Employee.ts
+++ src/migrations/1669075905477-Employee.ts
@@ -1,7 +1,49 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class Employee1669075905477 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'employee',
+        columns: [
+          {
+            name: 'employee_id',
+            type: 'uuid',
+            isUnique: true,
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'user_name',
+            type: 'varchar',
+            isUnique: true,
+            isNullable: false,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      })
+    );
+  }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('employee');
+  }
 }
