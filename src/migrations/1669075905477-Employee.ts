5552ef12f386e2d64104c54881e5d47b06352c48
Roberto Sousa <robertosousa186@gmail.com>
2022-11-21 22:30:42 -0300

    Creating first migration (Employee)

---
+++ src/migrations/1669075905477-Employee.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class Employee1669075905477 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
