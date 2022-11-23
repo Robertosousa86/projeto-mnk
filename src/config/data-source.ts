aeee844af21cc852b165d326779659516aab2764
Roberto Sousa <robertosousa186@gmail.com>
2022-11-23 20:57:43 -0300

    Updating data-source

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,27 +1,28 @@
+import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const AppDataSource = new DataSource({
+const appDataSource = new DataSource({
   type: 'mysql',
   host: process.env.DATABASE_HOST,
   port: +process.env.DATABASE_PORT,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE,
-  logging: true,
-  synchronize: true,
-  entities: [
-    /*...*/
-  ],
+  logging: ['info'],
+  entities: ['./src/entities/**/*.ts'],
   migrations: ['src/migrations/**/*{.ts,.js}'],
 });
 
-AppDataSource.initialize()
+appDataSource
+  .initialize()
   .then(() => {
     console.log('Data Source has been initialized!');
   })
   .catch((err) => {
-    console.error('Error during Data Source initialization.', err);
+    console.error('Error during Data Source initialization', err);
   });
+
+export default appDataSource;
