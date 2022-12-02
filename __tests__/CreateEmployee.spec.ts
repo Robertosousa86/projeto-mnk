1e53a7b53f49ce419f0742b61b362bab5136f308
Roberto Sousa <robertosousa186@gmail.com>
2022-12-01 23:52:41 -0300

    Creating tests for empty fields in requests

--- __tests__/CreateEmployee.spec.ts
+++ __tests__/CreateEmployee.spec.ts
@@ -1,6 +1,13 @@
 import request from 'supertest';
 import { appDataSource } from '../src/config/data-source';
+import { EmployeeType } from '../src/controllers/types/EmployeeType';
 import app from '../src/app';
+
+const fakeEmployee: EmployeeType = {
+  name: 'Tica',
+  user_name: 'Tica',
+  password: '123456',
+};
 
 beforeEach(async () => {
   await appDataSource.initialize();
@@ -11,12 +18,6 @@ afterEach(async () => {
     .destroy()
     .then(() => console.log('Connection was closed.'));
 });
-
-const fakeEmployee = {
-  name: 'Tica',
-  user_name: 'Tica',
-  password: '123456',
-};
 
 describe('Employee registration', () => {
   it.skip('Should be return "200 OK" when create request is valid.', async () => {
@@ -32,8 +33,50 @@ describe('Employee registration', () => {
   });
 
   it('Should be return "User name already exists." when user name is already registered.', async () => {
+    const fakeEmployee: EmployeeType = {
+      name: 'Tica',
+      user_name: 'Tica',
+      password: '123456',
+    };
+
     const result = await request(app).post('/employees').send(fakeEmployee);
 
     expect(result.body.message).toEqual('User name already exists.');
+  });
+
+  it('Should be return "User field is required." when user field is empty.', async () => {
+    const fakeEmployee: EmployeeType = {
+      name: '',
+      user_name: 'Tica',
+      password: '123456',
+    };
+
+    const result = await request(app).post('/employees').send(fakeEmployee);
+
+    expect(result.body.message).toBe('User field is required.');
+  });
+
+  it('Should be return "User Name field is required." when user_name field is empty.', async () => {
+    const fakeEmployee: EmployeeType = {
+      name: 'Tica',
+      user_name: '',
+      password: '123456',
+    };
+
+    const result = await request(app).post('/employees').send(fakeEmployee);
+
+    expect(result.body.message).toBe('User Name field is required.');
+  });
+
+  it('Should be return "User Name field is required." when password field is empty.', async () => {
+    const fakeEmployee: EmployeeType = {
+      name: 'Tica',
+      user_name: 'Tica',
+      password: '',
+    };
+
+    const result = await request(app).post('/employees').send(fakeEmployee);
+
+    expect(result.body.message).toBe('Password field is required.');
   });
 });
