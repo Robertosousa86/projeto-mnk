04a4967266bf948ef8062501ed2d0234b3bab679
Roberto Sousa <robertosousa186@gmail.com>
2022-12-06 22:30:01 -0300

    Creatig employee service

--- src/services/CreateEmployee.ts
+++ src/services/CreateEmployee.ts
@@ -1,12 +1,24 @@
 import { appDataSource } from '../config/data-source';
 import { Employee } from '../entities/Employee';
-import { EmployeeData } from '../controllers/types/Employee';
+import { EmployeeType } from '../controllers/types/EmployeeType';
 
 export class CreateEmployee {
-  async execute(employeeRequest: EmployeeData): Promise<Employee> {
+  async execute(employeeType: EmployeeType): Promise<Employee | Error> {
     const repository = appDataSource.getRepository(Employee);
 
-    const employee = repository.create(employeeRequest);
+    const employee = repository.create(employeeType);
+
+    if (!employee.name) return new Error('User field is required.');
+
+    if (!employee.user_name) return new Error('User Name field is required.');
+
+    if (!employee.password) return new Error('Password field is required.');
+
+    const queryResult = await repository.findOne({
+      where: { user_name: employee.user_name },
+    });
+
+    if (queryResult) return new Error('User name already exists.');
 
     await repository.save(employee);
 
