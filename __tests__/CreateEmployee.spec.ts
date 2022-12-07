7a1fd4f6b4136a1a1c4c775dd64e076f7d926c8c
Roberto Sousa <robertosousa186@gmail.com>
2022-12-06 22:20:35 -0300

    Create tests for employee registration

--- __tests__/CreateEmployee.spec.ts
+++ __tests__/CreateEmployee.spec.ts
@@ -1,82 +1,91 @@
 import request from 'supertest';
 import { appDataSource } from '../src/config/data-source';
 import { EmployeeType } from '../src/controllers/types/EmployeeType';
+import { Employee } from '../src/entities/Employee';
 import app from '../src/app';
 
-const fakeEmployee: EmployeeType = {
-  name: 'Tica',
-  user_name: 'Tica',
-  password: '123456',
-};
-
-beforeEach(async () => {
+beforeAll(async () => {
   await appDataSource.initialize();
 });
 
-afterEach(async () => {
-  await appDataSource
-    .destroy()
-    .then(() => console.log('Connection was closed.'));
+afterAll(async () => {
+  await appDataSource.getRepository(Employee).delete({ name: 'Tica' });
+
+  await appDataSource.destroy();
 });
 
 describe('Employee registration', () => {
-  it.skip('Should be return "200 OK" when create request is valid.', async () => {
-    const result = await request(app).post('/employees').send(fakeEmployee);
+  const fakeEmployee: EmployeeType = {
+    name: 'Tica',
+    user_name: 'Tica',
+    password: '123456',
+  };
 
-    expect(result.statusCode).toEqual(200);
+  const postEmployee = (employee = fakeEmployee): request.Test => {
+    const route = request(app).post('/employees');
+
+    return route.send(employee);
+  };
+
+  it('Should be return "200 OK" and "Employee registered." when create request is valid.', async () => {
+    const result = await postEmployee();
+
+    expect(result.statusCode).toBe(200);
+
+    expect(result.body.message).toEqual('Employee registered.');
+  });
+
+  it('Should saves the employee to database.', async () => {
+    const repository = appDataSource.getRepository(Employee);
+
+    const queryResult = await repository.find();
+
+    expect(queryResult.length).toEqual(1);
   });
 
   it('Should be return "400 bad request" when create request is invalid.', async () => {
-    const result = await request(app).post('/employees').send(fakeEmployee);
+    const result = await postEmployee({
+      name: '',
+      user_name: '',
+      password: '',
+    });
 
     expect(result.statusCode).toEqual(400);
   });
 
-  it('Should be return "User name already exists." when user name is already registered.', async () => {
-    const fakeEmployee: EmployeeType = {
-      name: 'Tica',
-      user_name: 'Tica',
-      password: '123456',
-    };
-
-    const result = await request(app).post('/employees').send(fakeEmployee);
-
-    expect(result.body.message).toEqual('User name already exists.');
-  });
-
-  it('Should be return "User field is required." when user field is empty.', async () => {
-    const fakeEmployee: EmployeeType = {
+  it('Should be return "Name field is required." when user field is empty.', async () => {
+    const result = await postEmployee({
       name: '',
       user_name: 'Tica',
       password: '123456',
-    };
+    });
 
-    const result = await request(app).post('/employees').send(fakeEmployee);
-
-    expect(result.body.message).toBe('User field is required.');
+    expect(result.body.message).toEqual('User field is required.');
   });
 
   it('Should be return "User Name field is required." when user_name field is empty.', async () => {
-    const fakeEmployee: EmployeeType = {
+    const result = await postEmployee({
       name: 'Tica',
       user_name: '',
       password: '123456',
-    };
-
-    const result = await request(app).post('/employees').send(fakeEmployee);
+    });
 
     expect(result.body.message).toBe('User Name field is required.');
   });
 
-  it('Should be return "User Name field is required." when password field is empty.', async () => {
-    const fakeEmployee: EmployeeType = {
+  it('Should be return "Password field is required." when password field is empty.', async () => {
+    const result = await postEmployee({
       name: 'Tica',
       user_name: 'Tica',
       password: '',
-    };
-
-    const result = await request(app).post('/employees').send(fakeEmployee);
+    });
 
     expect(result.body.message).toBe('Password field is required.');
+  });
+
+  it('Should be return "User name already exists." when user name is already registered.', async () => {
+    const result = await postEmployee();
+
+    expect(result.body.message).toEqual('User name already exists.');
   });
 });
