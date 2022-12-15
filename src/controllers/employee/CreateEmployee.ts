8811ebbaaa2ea0c2e1917757a23ebc36d3dc187d
Roberto Sousa <robertosousa186@gmail.com>
2022-12-15 19:14:21 -0300

    Updating controller employee

--- src/controllers/employee/CreateEmployee.ts
+++ src/controllers/employee/CreateEmployee.ts
@@ -2,16 +2,16 @@ import { Request, Response } from 'express';
 import { CreateEmployee } from '../../services/CreateEmployee';
 
 export class CreateEmployeeController {
-  async handle(req: Request, res: Response) {
-    const employee = req.body;
+  async handle(request: Request, response: Response): Promise<Response> {
+    const employee = request.body;
 
     const service = new CreateEmployee();
 
     const serviceResult = await service.execute(employee);
 
     if (serviceResult instanceof Error)
-      return res.status(400).json({ message: serviceResult.message });
+      return response.status(400).json({ message: serviceResult.message });
 
-    return res.status(200).send({ message: 'Employee registered.' });
+    return response.status(200).send({ message: 'Employee registered.' });
   }
 }
