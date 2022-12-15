e1b31c8db7a536316a62ae3757254be4b4f6b5f9
Roberto Sousa <robertosousa186@gmail.com>
2022-12-14 21:25:18 -0300

    Updating controller

--- src/controllers/employee/CreateEmployee.ts
+++ src/controllers/employee/CreateEmployee.ts
@@ -3,11 +3,11 @@ import { CreateEmployee } from '../../services/CreateEmployee';
 
 export class CreateEmployeeController {
   async handle(req: Request, res: Response) {
-    const { name, user_name, password } = req.body;
+    const employee = req.body;
 
     const service = new CreateEmployee();
 
-    const serviceResult = await service.execute({ name, user_name, password });
+    const serviceResult = await service.execute(employee);
 
     if (serviceResult instanceof Error)
       return res.status(400).json({ message: serviceResult.message });
